7936ec0c38d3e4d24d0b49f2261f85d1fa3b0737
qq1132416 <qq1132416@outlook.com>
2023-04-21 11:25:19 +0800

    fix: QuestionAnswerPrompt

--- 8. llama-index/llama-index.py
+++ 8. llama-index/llama-index.py
@@ -21,3 +21,23 @@ print(response)
 
 response = index.query("鲁迅先生是去哪里学的医学？", verbose=True)
 print(response)
+
+
+# 上述过程里面是如何提交给 OpenAI 的呢？
+from llama_index import QuestionAnswerPrompt
+query_str = "鲁迅先生去哪里学的医学？"
+
+# 模版里面支持两个变量，一个叫做 context_str，另一个叫做 query_str。context_str 的地方，在实际调用的时候，会被通过 Embedding 相似度找出来的内容填入。而 query_str 则是会被我们实际提的问题替换掉。
+# 实际提问的时候，我们告诉 AI，只考虑上下文信息，而不要根据自己已经有的先验知识（prior knowledge）来回答问题。
+DEFAULT_TEXT_QA_PROMPT_TMPL = (
+    "Context information is below. \n"
+    "---------------------\n"
+    "{context_str}"
+    "\n---------------------\n"
+    "Given the context information and not prior knowledge, "
+    "answer the question: {query_str}\n"
+)
+QA_PROMPT = QuestionAnswerPrompt(DEFAULT_TEXT_QA_PROMPT_TMPL)
+
+response = index.query(query_str, text_qa_template=QA_PROMPT)
+print(response)
