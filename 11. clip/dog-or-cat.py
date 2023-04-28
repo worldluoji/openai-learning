c4d0942be5d7b90bd39cfc15aa362f5fbd513e2e
qq1132416 <qq1132416@outlook.com>
2023-04-28 11:13:08 +0800

    feat: add dog or cat demo

---
+++ 11. clip/dog-or-cat.py
@@ -0,0 +1,40 @@
+
+import torch
+from PIL import Image
+from IPython.display import display
+from IPython.display import Image as IPyImage
+from transformers import CLIPProcessor, CLIPModel
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+def get_image_feature(filename: str):
+    image = Image.open(filename).convert("RGB")
+    processed = processor(images=image, return_tensors="pt", padding=True, truncation=True)
+    with torch.no_grad():
+        image_features = model.get_image_features(pixel_values=processed["pixel_values"])
+    return image_features
+
+def get_text_feature(text: str):
+    processed = processor(text=text, return_tensors="pt", padding=True, truncation=True)
+    with torch.no_grad():
+        text_features = model.get_text_features(processed['input_ids'])
+    return text_features
+
+def cosine_similarity(tensor1, tensor2):
+    tensor1_normalized = tensor1 / tensor1.norm(dim=-1, keepdim=True)
+    tensor2_normalized = tensor2 / tensor2.norm(dim=-1, keepdim=True)
+    return (tensor1_normalized * tensor2_normalized).sum(dim=-1)
+
+image_tensor = get_image_feature("./data/animal.jpeg")
+
+cat_text = "This is a cat."
+cat_text_tensor = get_text_feature(cat_text)
+
+dog_text = "This is a dog."
+dog_text_tensor = get_text_feature(dog_text)
+
+display(IPyImage(filename='./data/animal.jpeg'))
+
+print("Similarity with cat : ", cosine_similarity(image_tensor, cat_text_tensor))
+print("Similarity with dog : ", cosine_similarity(image_tensor, dog_text_tensor))
