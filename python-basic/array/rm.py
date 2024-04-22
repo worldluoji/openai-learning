d15403a9513b4ab0fe8245974ca5620977dd6532
worldluoji <qq1132416@outlook.com>
2024-04-22 14:41:32 +0800

    feat: add rm demo

---
+++ python-basic/array/rm.py
@@ -0,0 +1,14 @@
+
+l = [1,2,3,4]
+
+l.remove(1)
+
+print(l)
+print(l.index(2))
+
+try:
+    print(l.index(1))
+except ValueError as e:
+    print("not found")
+finally:
+    print("always implement")
